c7a201e9831fff84bf87a5d5f075e2f5a6997871
James Montgomery <james.montgomery179@gmail.com>
2021-04-23 22:26:32 -0500

    More button styles

--- src/components/parts/Button/index.tsx
+++ src/components/parts/Button/index.tsx
@@ -1,9 +1,19 @@
-import { RedButton, GreenButton, PinkButton } from './style'
+import {
+  RedButton,
+  GreenButton,
+  PinkButton,
+  WhiteButton,
+  TransparentButton,
+} from './style'
 
 const Button = ({ theme = 'red', thin = false, children }) => {
   if ('green' === theme)
     return <GreenButton thin={thin}>{children}</GreenButton>
   if ('pink' === theme) return <PinkButton thin={thin}>{children}</PinkButton>
+  if ('transparent' === theme)
+    return <TransparentButton thin={thin}>{children}</TransparentButton>
+  if ('white' === theme)
+    return <WhiteButton thin={thin}>{children}</WhiteButton>
   return <RedButton thin={thin}>{children}</RedButton>
 }
 
--- src/components/parts/Button/style.ts
+++ src/components/parts/Button/style.ts
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 import { colors } from 'src/theme'
 
 const Button = styled.button`
-  padding: ${({ thin }) => (thin ? '5px 8px' : '10px 30px')};
+  padding: ${({ thin }) => (thin ? '5px 8px' : '8px 40px')};
   font-size: 19px;
   line-height: 26px;
   border-radius: 6px;
   text-transform: uppercase;
-  border: 1px solid transparent;
+  border-width: 1px;
+  border-style: solid;
+  cursor: pointer;
 `
 
 export const RedButton = styled(Button)`
@@ -26,6 +28,12 @@ export const WhiteButton = styled(Button)`
   color: ${colors.black};
   background: ${colors.white};
   border-color: ${colors.red};
+`
+
+export const TransparentButton = styled(Button)`
+  color: ${colors.white};
+  background: transparent;
+  border-color: ${colors.white};
 `
 
 export const PinkButton = styled(Button)`
