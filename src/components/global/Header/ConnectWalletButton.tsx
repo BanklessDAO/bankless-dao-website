23f5f59c178345bd558f0e3717e67bf40db35f8b
James Montgomery <james.montgomery179@gmail.com>
2021-05-03 21:24:46 -0500

    Adds token modal Adds whale util

--- src/components/global/Header/ConnectWalletButton.tsx
+++ src/components/global/Header/ConnectWalletButton.tsx
@@ -1,14 +1,20 @@
 import Web3Modal from 'web3modal'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import Button from 'src/components/parts/Button'
 import { useWalletWeb3React } from 'src/hooks'
-import { injected, shortenAddress } from 'src/utils'
+import { useDetectOutsideClick } from 'src/hooks/useDetectOutsideClick'
+import { injected, shortenAddress, trimCurrencyForWhales } from 'src/utils'
+
+import { TokenModal } from './ConnectWalletModal/style'
 
 const WalletButton = () => {
   const walletWeb3ReactContext = useWalletWeb3React()
   const isConnected = walletWeb3ReactContext.active
   const [connectClick, setConnectClick] = useState(false)
+
+  const modalRef = useRef(null)
+  const [modalOpen, setModalOpen] = useDetectOutsideClick(modalRef, false)
 
   useEffect(() => {
     if (connectClick) {
@@ -35,20 +41,63 @@ const WalletButton = () => {
         .catch()
     }
   }, [connectClick])
-  console.log(walletWeb3ReactContext)
+
+  // useEffect(() => {
+  //   console.log(modalOpen)
+  // }, [modalOpen])
+  const rawBalance = 1231312300
+  const commaBalance = Number(rawBalance).toLocaleString('en')
+  const whaleBalance = trimCurrencyForWhales(rawBalance)
+
   return (
-    <Button
-      theme={isConnected ? 'green' : 'pink'}
-      thin={true}
-      uppercase={false}
-      onClick={() => {
-        isConnected ? undefined : setConnectClick(true)
-      }}
-    >
-      {isConnected
-        ? shortenAddress(walletWeb3ReactContext.account)
-        : 'Connect a Wallet'}
-    </Button>
+    <React.Fragment>
+      <Button
+        theme={isConnected ? 'green' : 'pink'}
+        thin={true}
+        uppercase={false}
+        onClick={() => {
+          isConnected ? setModalOpen(!modalOpen) : setConnectClick(true)
+        }}
+      >
+        {isConnected
+          ? shortenAddress(walletWeb3ReactContext.account)
+          : 'Connect a Wallet'}
+      </Button>
+      <TokenModal ref={modalRef} active={modalOpen}>
+        <TokenModal.TitleRow>
+          <TokenModal.Title>Your BANK Breakdown</TokenModal.Title>
+          <TokenModal.Close
+            onClick={() => setModalOpen(!modalOpen)}
+            src="/images/icon-close.svg"
+            height="30px"
+            width="30px"
+            alt="close modal"
+          />
+        </TokenModal.TitleRow>
+        <TokenModal.BigRow>
+          <img
+            src="/images/token-3d.png"
+            height="90px"
+            width="90px"
+            alt="3d token icon"
+          />
+          <strong>{whaleBalance}</strong>
+        </TokenModal.BigRow>
+        <TokenModal.BalanceRow>
+          <TokenModal.StatLine>
+            Balance: <span>{commaBalance} BANK</span>
+          </TokenModal.StatLine>
+          <TokenModal.StatLine>
+            Unclaimed: <span>0 BANK</span>
+          </TokenModal.StatLine>
+        </TokenModal.BalanceRow>
+        <TokenModal.SupplyRow>
+          <TokenModal.StatLine>
+            Total supply: <span>1B BANK</span>
+          </TokenModal.StatLine>
+        </TokenModal.SupplyRow>
+      </TokenModal>
+    </React.Fragment>
   )
 }
 
