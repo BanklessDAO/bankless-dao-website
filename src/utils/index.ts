4bdf56abf7d054b50f3af4c5844707f7e5a1021d
Oktalize <oxta@protonmail.com>
2021-05-27 14:35:41 -0500

    Change trim currency to 1 decimal place for M and B

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -88,10 +88,10 @@ export const toFixed = function (x) {
 export const trimCurrencyForWhales = (labelValue) => {
   // Nine Zeroes for Billions
   return Math.abs(Number(labelValue)) >= 1.0e9
-    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + 'B'
+    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(1) + 'B'
     : // Six Zeroes for Millions
     Math.abs(Number(labelValue)) >= 1.0e6
-    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + 'M'
+    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(1) + 'M'
     : // Three Zeroes for Thousands
     Math.abs(Number(labelValue)) >= 1.0e3
     ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + 'K'
