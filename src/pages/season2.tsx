1434393cfcdc19aea053d78e07b2ab6e417473ef
Didier Krux <didier@krux.co>
2021-09-16 13:43:43 +0300

    fix season2 hack

--- src/pages/season2.tsx
+++ src/pages/season2.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import Router from 'next/router'
-
-// this is a hackery to provide a easy url for season 2 notion page. next.config.js would be better for this but it doesn't exist here so this trick is used//
-const Season2 = (): any => {
-  useState(() => {
-    const { pathname } = Router
-    if (pathname == '/season2') {
-      Router.push(
-        'https://www.notion.so/bankless/Season-2-Launch-7d06aaf56df444d48cd0d551edadebdc'
-      )
-    }
-  })
-
+const Season2 = (): JSX.Element => {
+  // HACK: redirect to Notion
+  window.location.replace(
+    'https://www.notion.so/bankless/Season-2-Launch-7d06aaf56df444d48cd0d551edadebdc'
+  )
   return null
 }
 
